e002dee6ba1e660479541a97928db54e97a782fc
Babak Taghizadeh <babak.tz98@gmail.com>
2025-03-06 15:30:53 +0330

    Modify links to be wrapped with MUI Button component for more consistency

--- src/components/page-layout/header/menuDrawer.tsx
+++ src/components/page-layout/header/menuDrawer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Drawer, IconButton, List, ListItem } from "@mui/material";
+import { Box, Button, Drawer, IconButton, List, ListItem } from "@mui/material";
 import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
@@ -8,7 +8,6 @@ import { usePathname } from "next/navigation";
 
 const MenuDrawer = () => {
   const pathName = usePathname();
-  console.log(pathName);
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const toggleDrawer = (newOpen: boolean) => setIsOpen(newOpen);
@@ -22,18 +21,18 @@ const MenuDrawer = () => {
       >
         <MenuIcon sx={{ color: "#1158A7" }} />
       </IconButton>
-      <Drawer anchor="right" open={isOpen} onClose={() => toggleDrawer(false)}>
+      <Drawer anchor="right" open={isOpen} onClose={() => toggleDrawer(false)} >
         <Box sx={{ width: 250 }} onClick={() => toggleDrawer(false)}>
           <List>
             {NAV_LINKS.map((item) => (
-              <ListItem key={item.id} sx={{color: pathName === item.href ? "red" : "inherit" }}>
-                <Link href={item.href}>{item.label}</Link>
-                {/* <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton> */}
+              <ListItem
+                key={item.id}
+                sx={{
+                  color: pathName === item.href ? "primary.main" : "inherit",
+                  borderBottom: "1px solid black",
+                }}
+              >
+                <Button component={Link} href={item.href}>{item.label}</Button>
               </ListItem>
             ))}
           </List>
