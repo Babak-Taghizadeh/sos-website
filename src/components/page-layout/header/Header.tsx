13a6a4f4a3d1a83351bbc55629fd6fda029fc082
Babak Taghizadeh <babak.tz98@gmail.com>
2025-03-06 15:32:29 +0330

    Split menu header from Header component for more readability and consistency

--- src/components/page-layout/header/Header.tsx
+++ src/components/page-layout/header/Header.tsx
@@ -1,14 +1,22 @@
-import { AppBar, Button, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import Image from "next/image";
 import Logo from "@/public/images/sos-logo.png";
 import MenuDrawer from "./menuDrawer";
+import MenuHeader from "./menuHeader";
 
 const Header = () => {
   return (
-    <AppBar position="sticky" color="secondary" sx={{ flexDirection: "row" }}>
+    <AppBar
+      position="sticky"
+      color="secondary"
+      sx={{ flexDirection: "row", px: { lg: 4 } }}
+    >
       <Toolbar sx={{ width: "100%", justifyContent: "space-between" }}>
-        <MenuDrawer />
+        <Box sx={{ display: { xs: "block", md: "none" } }}>
+          <MenuDrawer />
+        </Box>
         <Image src={Logo} alt="logo" quality={100} />
+        <MenuHeader />
         <Button variant="contained">ورود و فعالسازی</Button>
       </Toolbar>
     </AppBar>
---
+++ src/components/page-layout/header/menuHeader.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { NAV_LINKS } from "@/constants/headerLinks";
+import { Button, List, ListItem } from "@mui/material";
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+
+const MenuHeader = () => {
+  const pathName = usePathname();
+
+  return (
+    <List
+      sx={{
+        display: { xs: "none", md: "flex" },
+        flexDirection: "row",
+        justifyContent: "space-between",
+        flexGrow: 1,
+        mx: { md: 4, lg: 8, xl: 12 },
+      }}
+      disablePadding
+    >
+      {NAV_LINKS.map((item) => (
+        <ListItem
+          key={item.id}
+          disablePadding
+          sx={{
+            width: "auto",
+            color: pathName === item.href ? "primary.main" : "inherit",
+          }}
+        >
+          <Button
+            sx={(theme) => ({
+              borderBottom:
+                pathName === item.href
+                  ? `1px solid ${theme.palette.primary.main}`
+                  : "none",
+            })}
+            component={Link}
+            href={item.href}
+          >
+            {item.label}
+          </Button>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default MenuHeader;
