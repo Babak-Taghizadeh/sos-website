c55b0acb30e9db5e72b797bbc6960a905ed180fc
Babak Taghizadeh <babak.tz98@gmail.com>
2025-03-06 20:20:40 +0330

    Add not found article component for better user experience when attempting to reach not excisted article id

---
+++ src/components/article/notFoundArticle.tsx
@@ -0,0 +1,26 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+import BackButton from "./backButton";
+
+const NotFoundArticle = () => {
+  return (
+    <Container
+      sx={{
+        height: "70vh",
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        alignItems: "center",
+        justifyContent: "center",
+        mt: 4,
+      }}
+    >
+      <Typography variant="h5" textAlign="center" mt={5}>
+        مقاله پیدا نشد!
+      </Typography>
+      <BackButton title="بازگشت به صفحه اصلی" />
+    </Container>
+  );
+};
+
+export default NotFoundArticle;
