2d11a012f548be90c5d638c2c92d89a0c1348926
Babak Taghizadeh <babak.tz98@gmail.com>
2025-03-08 01:38:02 +0330

    Fix style issues when title gets longer

--- src/components/todos/todoList/todoCard.tsx
+++ src/components/todos/todoList/todoCard.tsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 import { Todo } from "@/types/types";
-import { Button, Card, CardContent, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 
 interface TodoCardProps {
@@ -11,18 +11,35 @@ interface TodoCardProps {
 
 const TodoCard = ({ todo, handleEdit, handleDelete }: TodoCardProps) => {
   return (
-    <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
-      <Card>
-        <CardContent>
-          <Typography variant="h6">{todo.title}</Typography>
-          <Typography>Priority: {todo.priority}</Typography>
-          <Typography>Due Date: {todo.dueDate}</Typography>
-          <Button onClick={() => handleEdit(todo)} color="primary">
-            Edit
-          </Button>
-          <Button onClick={() => handleDelete(todo.id)} color="error">
-            Delete
-          </Button>
+    <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+      <Card
+        sx={{ minHeight: "13rem", display: "flex", flexDirection: "column" }}
+      >
+        <CardContent
+          sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}
+        >
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h6">{todo.title}</Typography>
+            <Typography>Priority: {todo.priority}</Typography>
+            <Typography>Due Date: {todo.dueDate}</Typography>
+          </Box>
+
+          <Box sx={{ mt: "auto", pt: 1, display: "flex", gap: 1 }}>
+            <Button
+              onClick={() => handleEdit(todo)}
+              color="primary"
+              variant="outlined"
+            >
+              Edit
+            </Button>
+            <Button
+              onClick={() => handleDelete(todo.id)}
+              color="error"
+              variant="outlined"
+            >
+              Delete
+            </Button>
+          </Box>
         </CardContent>
       </Card>
     </Grid>
