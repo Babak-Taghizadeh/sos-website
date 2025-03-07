6ddc110f57fca5ca5a77b751c34cce12e8d1369f
Babak Taghizadeh <babak.tz98@gmail.com>
2025-03-07 18:20:23 +0330

    Add todo's types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,15 @@
 export interface ArticlesProps {
-    id: number;
-    title: string;
-    readTime: string;
-    description: string;
-    image: string;
-  }
-  
+  id: number;
+  title: string;
+  readTime: string;
+  description: string;
+  image: string;
+}
+
+export interface Todo {
+  id: number;
+  title: string;
+  priority: "High" | "Medium" | "Low";
+  dueDate: string;
+  completed: boolean;
+}
