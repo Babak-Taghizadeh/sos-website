5c6ed8dd57ab817f2a769ea3c260508dc3740be0
Babak Taghizadeh <babak.tz98@gmail.com>
2025-03-07 18:21:11 +0330

    Add todo related server actions for mutations

---
+++ src/utils/todoActions.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { Todo } from "../types/types";
+
+const API_URL = "http://localhost:4000";
+
+export const addTodo = async (todo: Omit<Todo, "id">): Promise<Todo> => {
+  const response = await fetch(`${API_URL}/todos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to add todo");
+  }
+  revalidatePath("todos");
+
+  return response.json();
+};
+
+export const updateTodo = async (
+  id: number,
+  todo: Partial<Todo>
+): Promise<Todo> => {
+  const response = await fetch(`${API_URL}/todos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to update todo");
+  }
+  revalidatePath("todos");
+  return response.json();
+};
+
+export const deleteTodo = async (id: number): Promise<void> => {
+  const response = await fetch(`${API_URL}/todos/${id}`, {
+    method: "DELETE",
+  });
+  if (!response.ok) {
+    throw new Error("Failed to delete todo");
+  }
+  revalidatePath("todos");
+};
